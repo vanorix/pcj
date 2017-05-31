2db125a4f61b62d4922541a8d403cb57dd0bfc8c
Javier Castillo <javiercastillo2708@gmail.com>
2017-05-31 14:07:21 -0400

    Fixed: - ActionDispatch::Cookies::CookieOverflow by changing from cookie_store to active_record_store to expand 4KB cookie size limit.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,6 @@ class AuthController < ApplicationController
     token = get_token_from_code params[:code]
     session[:azure_token] = token.to_hash
     session[:user_mail] = get_user_email token.token
-    render text: "Email: #{email}, TOKEN: #{token.token}. SAVED in session cookie."
+    render text: "SAVED in session cookie."
   end
 end
